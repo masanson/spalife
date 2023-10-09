147ce00cf08104329c44f1ba99a341e6dfaf5daa
masanson <138214683+masanson@users.noreply.github.com>
2023-10-09 12:07:27 +0000

    [Update] enum設定完了

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -6,12 +6,14 @@ class EndUser < ApplicationRecord
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visitor_id', dependent: :destroy
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
   
+  enum sex: { male: 0, female: 1, not_applicable: 2 }
+  
   has_one_attached :user_image
   
   def get_user_image(width, height)
     unless user_image.attached?
-      file_path = Rails.root.join('app/assets/images/user_defalt_image')
-      user_image.attach(io: File.open(file_path), filename: 'defauit-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/user_default_image.jpg')
+      user_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     user_image.variant(resize_to_limit: [width, height]).processed
   end
