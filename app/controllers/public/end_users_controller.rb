10499ec7d1567b9d99c3c75e863275daca16f642
masanson <138214683+masanson@users.noreply.github.com>
2023-10-11 12:40:23 +0000

    [Update] マイページ設計1

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,10 +1,17 @@
 class Public::EndUsersController < ApplicationController
   def show
+    @end_user =EndUser.find(params[:id])
   end
 
   def edit
   end
 
   def withdrawal
+  end
+  
+  private
+  
+  def end_user_params
+    params.require(:end_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :sex, :email, :postal_code, :address, :telephone_number)
   end
 end
