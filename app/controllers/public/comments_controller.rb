7f5c6cd3d005cf9bf6fb005e5441b696784f6125
masanson <138214683+masanson@users.noreply.github.com>
2023-10-19 12:47:48 +0000

    [Update] コメント機能作成2_POST＆Destroy

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,16 +1,16 @@
 class Public::CommentsController < ApplicationController
   def create
-    @hot_post = HotPost.find_by(id: params[:comment][:hot_post_id])
-    @comment = Comment.new(content: params[:content], end_user_id: current_end_user.id, hot_post_id: params[:hot_post_id])
+    @hot_post = HotPost.find_by(params[:hot_post_id])
+    @comment = Comment.new(comment_params)
     @comment.save
     redirect_to public_hot_post_path(@hot_post.id)
   end
   
   def destroy
-     @hot_post = HotPost.find_by(params[:hot_post_id])
-     @comment = Comment.find(params[:id])
-     @comment.destroy
-     redirect_to public_hot_post_path(@hot_post.id)
+   @hot_post = HotPost.find_by(params[:hot_post_id])
+   @comment = Comment.find_by(params[:id])
+   @comment.destroy
+   redirect_to public_hot_post_path(@hot_post.id)
   end
   
   private
