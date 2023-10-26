e2928a154d68f43e1582b08ca9ae89bca692a10b
masanson <138214683+masanson@users.noreply.github.com>
2023-10-26 12:45:03 +0000

    [Update] 温泉地作成3_indes,show,edit作成_途中

--- app/controllers/admin/hot_springs_controller.rb
+++ app/controllers/admin/hot_springs_controller.rb
@@ -1,14 +1,41 @@
 class Admin::HotSpringsController < ApplicationController
   def index
+    @hot_springs = HotSpring.all
     @hot_spring = HotSpring.new
   end
 
+  def create
+    @hot_spring = HotSpring.new(hot_spring_params)
+    if @hot_spring.save
+      redirect_to admin_hot_spring_path(@hot_spring.id)
+    else
+      render :index
+    end
+  end
+
   def show
+    @hot_spring = HotSpring.find(params[:id])
   end
 
   def edit
+    @hot_spring = HotSpring.find(params[:id])
   end
-  
+
+  def update
+    @hot_spring = HotSpring.find(params[:id])
+    if @hot_spring.update(hot_spring_params)
+      redirect_to admin_hot_spring_path(@hot_spring.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @hot_spring = HotSpring.find(params[:id])
+    @hot_spring.destroy
+    redirect_to admin_hot_springs_path
+  end
+
   private
   
   def hot_spring_params
--- app/models/hot_spring.rb
+++ app/models/hot_spring.rb
@@ -4,6 +4,10 @@ class HotSpring < ApplicationRecord
   
   has_one_attached :hot_spring_image
   
+  def address_full
+    @hot_spring.prefecture.name + '' + address
+  end
+  
   def get_hot_spring_image(width, height)
     hot_spring_image.variant(resize_to_limit: [width, height]).processed
   end
