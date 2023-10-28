6ffcd169cc8e3c00f41b6295b9bdb25f7e34bef5
masanson <138214683+masanson@users.noreply.github.com>
2023-10-28 14:02:50 +0000

    [Update] 温泉地作成5_地図機能作成1

--- app/models/hot_spring.rb
+++ app/models/hot_spring.rb
@@ -1,13 +1,13 @@
 class HotSpring < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  
+
   has_one_attached :hot_spring_image
-  
+
   def address_full
-    '@hot_spring.find_by(Prefecture.name: :prefecture_id)'+' '+ address
+    prefecture.name + address
   end
-  
+
   def get_hot_spring_image(width, height)
     hot_spring_image.variant(resize_to_limit: [width, height]).processed
   end
