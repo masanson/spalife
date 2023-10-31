e4dcf55adca740c98cb1d10a930839b4798c4652
masanson <138214683+masanson@users.noreply.github.com>
2023-10-31 12:06:14 +0000

    [Update] 温泉地作成6_public表示

--- app/controllers/public/hot_springs_controller.rb
+++ app/controllers/public/hot_springs_controller.rb
@@ -1,7 +1,19 @@
 class Public::HotSpringsController < ApplicationController
   def index
+    @hot_springs = HotSpring.all
   end
 
   def show
+    @hot_spring = HotSpring.find(params[:id])
+    search_address = (@hot_spring.address_full).gsub(/[\d-]+/, '')
+    results = Geocoder.search(search_address)
+    @latlng = results.first.coordinates
   end
+  
+  private
+  
+  def hot_spring_params
+    params.require(:hot_spring).permit(:hot_spring_image, :name, :introduction, :postal_code, :address, :telephone_number, :access, :prefecture_id)
+  end
+  
 end
