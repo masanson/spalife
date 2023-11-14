eaa923b472289d3452091ba26dc4aab87b080949
masanson <138214683+masanson@users.noreply.github.com>
2023-11-14 12:13:39 +0000

    [Update] レイアウト_end_user_show3_add:introduction_column,

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -37,6 +37,6 @@ class Public::EndUsersController < ApplicationController
   private
   
   def end_user_params
-    params.require(:end_user).permit(:user_image, :last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :sex, :email, :postal_code, :address, :telephone_number)
+    params.require(:end_user).permit(:user_image, :last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :sex, :email, :postal_code, :address, :telephone_number, :introduction)
   end
 end
