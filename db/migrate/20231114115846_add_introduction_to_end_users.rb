b427b97b0572329ccb3eb7663a39e7e9f6dca2f0
masanson <138214683+masanson@users.noreply.github.com>
2023-11-15 12:30:07 +0000

    [Update] レイアウト_end_user_show4

--- db/migrate/20231114115846_add_introduction_to_end_users.rb
+++ db/migrate/20231114115846_add_introduction_to_end_users.rb
@@ -1,5 +1,5 @@
 class AddIntroductionToEndUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :end_users, :introduction, :text
+    add_column :end_users, :introduction, :text, default: 'よろしくお願いします。'
   end
 end
