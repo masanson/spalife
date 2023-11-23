49d3f363cea0907eebcc5cbc4931815a8e51b078
masanson <138214683+masanson@users.noreply.github.com>
2023-11-23 13:28:46 +0000

    [Update] レイアウト_end_user_show7,admin_genre3

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,7 +1,13 @@
 class Admin::EndUsersController < ApplicationController
   def show
-    @end_user = EndUser.find(params[:id])
+    @end_user =EndUser.find(params[:id])
     @hot_posts = @end_user.hot_posts
+    @published_posts =@hot_posts.where(status: "published").order(created_at: :desc).page(params[:page]).per(5)
+    @draft_posts = @hot_posts.where(status: "draft").order(created_at: :desc).page(params[:page]).per(5)
+    @unpublished_posts = @hot_posts.where(status: "unpublished").order(created_at: :desc).page(params[:page]).per(5)
+    favorites = Favorite.where(end_user_id: @end_user.id).pluck(:hot_post_id)
+    @favorite_posts = HotPost.find(favorites)
+    @favorite_posts = Kaminari.paginate_array(@favorite_posts).page(params[:page]).per(5)
   end
 
   def edit
@@ -20,6 +26,6 @@ class Admin::EndUsersController < ApplicationController
   private
   
   def end_user_params
-    params.require(:end_user).permit(:user_image, :last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :sex, :email, :postal_code, :address, :telephone_number, :is_active)
+    params.require(:end_user).permit(:user_image, :last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :sex, :email, :postal_code, :address, :telephone_number, :is_active, :introduction)
   end
 end
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
-    @end_users = EndUser.all
+    @end_users = EndUser.order(created_at: :desc).page(params[:page]).per(8)
   end
 end
