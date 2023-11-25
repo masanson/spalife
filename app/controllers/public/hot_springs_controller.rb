d50976007e80aab14e0cb381665c12efda067081
masanson <138214683+masanson@users.noreply.github.com>
2023-11-25 13:35:25 +0000

    [Update] レイアウト_notification&end_user_show&hot_post_index

--- app/controllers/public/hot_springs_controller.rb
+++ app/controllers/public/hot_springs_controller.rb
@@ -1,10 +1,14 @@
 class Public::HotSpringsController < ApplicationController
   def index
-    @hot_springs = HotSpring.all
+    @hot_springs = HotSpring.order(created_at: :desc).page(params[:page]).per(8)
+    @hot_springs = @hot_springs.where('name LIKE ?', "%#{params[:search]}%") if params[:search].present?
+    @hot_springs = @hot_springs.where(prefecture_id: params[:prefecture_id]) if params[:prefecture_id].present?
+    @hot_spring = HotSpring.new
   end
 
   def show
     @hot_spring = HotSpring.find(params[:id])
+    @hot_posts = @hot_spring.hot_posts.page(params[:page]).per(4).order(created_at: :desc)
     search_address = (@hot_spring.address_full).gsub(/[\d-]+/, '')
     results = Geocoder.search(search_address)
     @latlng = results.first.coordinates
--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,6 +1,6 @@
 class Public::NotificationsController < ApplicationController
   def index
-    @notifications = current_end_user.passive_notifications.page(params[:page]).per(5)
+    @notifications = current_end_user.passive_notifications.order(created_at: :desc).page(params[:page]).per(5)
     @notifications.where(checked: false).each do |notification|
       notification.update(checked: true)
     end
