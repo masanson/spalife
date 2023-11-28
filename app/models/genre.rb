0b7fbb3a6f9f178ba48667fd592152c5e9a093fd
masanson <138214683+masanson@users.noreply.github.com>
2023-11-28 13:28:48 +0000

    [Update] バリデーション調整

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
   has_many :hot_posts, dependent: :destroy
+  validates :name, presence: { message: "が無記入です。" }
 end
--- app/models/hot_spring.rb
+++ app/models/hot_spring.rb
@@ -3,6 +3,13 @@ class HotSpring < ApplicationRecord
   belongs_to_active_hash :prefecture
   has_many :hot_posts, dependent: :destroy
 
+  validates :name, presence: { message: "が無記入です。" }
+  validates :introduction, presence: { message: "が無記入です。" }
+  validates :postal_code, presence: { message: "が無記入です。" }
+  validates :address, presence: { message: "が無記入です。" }
+  validates :telephone_number, presence: { message: "が無記入です。" }
+  validates :access, presence: { message: "が無記入です。" }
+
   has_one_attached :hot_spring_image
 
   def address_full
